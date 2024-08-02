dd6ed04a3173fa4ca56493ac43bf0290c5f2dd8d
pranaydeep1 <pranaydeep_korada@srmap.edu.in>
2024-08-02 19:12:36 +0530

    Add /bfhl prefix to endpoints

--- index.js
+++ index.js
@@ -1,9 +1,41 @@
+// const express = require('express');
+// const app = express();
+
+// app.use(express.json());
+
+// app.post('/endpoint', (req, res) => {
+//     const data = req.body;
+//     const response = {
+//         status: 'success',
+//         user_id: data.user_id,
+//         college_email_id: data.college_email_id,
+//         college_roll_number: data.college_roll_number,
+//         numbers: data.numbers,
+//         alphabets: data.alphabets
+//     };
+//     res.json(response);
+// });
+
+// app.get('/endpoint', (req, res) => {
+//     res.json({ operation_code: '12345' });
+// });
+
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}`);
+// });
+
 const express = require('express');
 const app = express();
 
 app.use(express.json());
 
-app.post('/endpoint', (req, res) => {
+// Define routes with /bfhl prefix
+app.get('/bfhl', (req, res) => {
+    res.send('Welcome to the API');
+});
+
+app.post('/bfhl/endpoint', (req, res) => {
     const data = req.body;
     const response = {
         status: 'success',
@@ -16,7 +48,7 @@ app.post('/endpoint', (req, res) => {
     res.json(response);
 });
 
-app.get('/endpoint', (req, res) => {
+app.get('/bfhl/endpoint', (req, res) => {
     res.json({ operation_code: '12345' });
 });
 
